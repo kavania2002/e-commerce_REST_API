dc6d4aa96f8e9001ea2cb35523ee33564fc6fcf6
kavania2002 <kavan155gondalia@gmail.com>
2022-05-22 15:25:45 +0530

    list of products from a specific seller

--- routes/buyer.js
+++ routes/buyer.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const passport = require("passport");
 
 const User = require("../models/user");
+const Catalog = require("../models/catalog");
+const Product = require("../models/product");
 
 router.get("/list-of-sellers", (req, res) => {
   if (req.isAuthenticated() && req.user.buyer) {
@@ -14,7 +16,31 @@ router.get("/list-of-sellers", (req, res) => {
       res.send(sellerName);
     });
   } else {
-      res.send('You are not authorized to see the sellers');
+    res.send("You are not authorized to see the sellers");
+  }
+});
+
+router.get("/seller-catalog/:seller_id", (req, res) => {
+  if (req.isAuthenticated() && req.user.buyer) {
+    Catalog.findOne({ seller: req.params.seller_id }, (err, seller) => {
+      if (err) console.log(err);
+      else if (!seller) res.send(`Such Seller doesn't exists`);
+      else {
+        const productIDs = seller.products;
+        let products = [];
+        productIDs.forEach((id) => {
+          Product.findById(id, (err, product) => {
+            products.push(product.name);
+            if (products.length == productIDs.length) {
+              console.log(products);
+              res.send(products);
+            }
+          });
+        });
+      }
+    });
+  } else {
+    res.send("You are not authorized to see the seller catalog");
   }
 });
 
